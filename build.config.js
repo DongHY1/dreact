c4e035774e3ed4be3a19dd813792c6ddedebf8e4
david dong <hi@daviddong.me>
2023-11-20 17:32:53 +0800

    feat: update config

--- build.config.js
+++ build.config.js
@@ -4,9 +4,9 @@ export default defineBuildConfig({
   entries: [
     {
       builder: "rollup",
+      name:'jsx-dev-runtime',
       input: "./src/react/jsx",
-      outDir: "./dist",
-      name:'jsx-dev-runtime'
+      outDir: "./dist"
     }
   ]
 });
